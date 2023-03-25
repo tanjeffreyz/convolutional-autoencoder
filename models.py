e99e55d945cf8ef680871317ad601f445ac324e0
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-03-24 17:53:00 -0700

    training pipeline works

--- models.py
+++ models.py
@@ -8,8 +8,9 @@ class ShapeProbe(nn.Module):
         return x
 
 
-class SimpleAutoencoder:
+class SimpleAutoencoder(nn.Module):
     def __init__(self, in_size):
+        super().__init__()
         assert in_size % 2 == 0, 'Dimensions must be divisible by 2'
 
         self.in_size = in_size
@@ -17,16 +18,18 @@ class SimpleAutoencoder:
         self.model = nn.Sequential(
             nn.Flatten(),
             nn.Linear(size, size // 2),
-            nn.Linear(size // 2, size)
+            nn.Linear(size // 2, size),
+            nn.Sigmoid()
         )
 
-    def __call__(self, x):
+    def forward(self, x):
         result = self.model.forward(x)
         return torch.reshape(result, (result.shape[0], 1, self.in_size, self.in_size))
 
 
-class ConvolutionalAutoencoder:
+class ConvolutionalAutoencoder(nn.Module):
     def __init__(self, _):      # in_size doesn't matter for CAE
+        super().__init__()
         self.model = nn.Sequential(
             # Encoding
             nn.Conv2d(1, 2, kernel_size=3, padding=1),
@@ -42,8 +45,9 @@ class ConvolutionalAutoencoder:
             # Decoding
             nn.ConvTranspose2d(8, 4, kernel_size=4, stride=2, padding=1),
             nn.ConvTranspose2d(4, 2, kernel_size=2, stride=2, padding=0),
-            nn.ConvTranspose2d(2, 1, kernel_size=4, stride=2, padding=1)
+            nn.ConvTranspose2d(2, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
         )
 
-    def __call__(self, x):
+    def forward(self, x):
         return self.model.forward(x)
--- train.py
+++ train.py
@@ -19,17 +19,23 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 writer = SummaryWriter()
 now = datetime.now()
 
+transform = T.Compose([
+    T.ToTensor(),
+    T.Resize((64, 64)),
+    T.Grayscale(),
+])
 train_set = LFWPeople(
     root='data',
     split='train',
     download=True,
-    transform=T.Compose([
-        T.ToTensor(),
-        T.Resize((64, 64)),
-        T.Grayscale(),
-    ])
+    transform=transform
 )
-test_set = LFWPeople(root='data', split='test', download=True)
+test_set = LFWPeople(
+    root='data',
+    split='test',
+    download=True,
+    transform=transform
+)
 
 train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=64)
@@ -37,13 +43,16 @@ test_loader = DataLoader(test_set, batch_size=64)
 # Initialize models
 x = next(iter(train_loader))[0]
 model_class = (ConvolutionalAutoencoder if args.conv else SimpleAutoencoder)
-model = model_class(x.shape[-1])
+model = model_class(x.shape[-1]).to(device)
 
-#
-print(x.shape)
-model(x)
-# print(model(x).shape)
+# Optimizer
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=1E-4
+)
 
+# Loss Function
+loss_function = torch.nn.BCELoss()
 
 # Create folders for this run
 root = os.path.join(
@@ -55,3 +64,51 @@ root = os.path.join(
 weight_dir = os.path.join(root, 'weights')
 if not os.path.isdir(weight_dir):
     os.makedirs(weight_dir)
+
+# Metrics
+train_losses = np.empty((2, 0))
+test_losses = np.empty((2, 0))
+
+
+def save_metrics():
+    np.save(os.path.join(root, 'train_losses'), train_losses)
+    np.save(os.path.join(root, 'test_losses'), test_losses)
+
+
+# Train
+for epoch in tqdm(range(25), desc='Epoch'):
+    model.train()
+    train_loss = 0
+    for data, _ in tqdm(train_loader, desc='Train', leave=False):
+        data = data.to(device)
+
+        optimizer.zero_grad()
+        predictions = model.forward(data)
+        loss = loss_function(predictions, data)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item() / len(train_loader)
+        del data
+
+    train_losses = np.append(train_losses, [[epoch], [train_loss]])
+    writer.add_scalar('Loss/train', train_loss, epoch)
+
+    if epoch % 4 == 0:
+        model.eval()
+        with torch.no_grad():
+            test_loss = 0
+            for data, _ in tqdm(test_loader, desc='Test', leave=False):
+                data = data.to(device)
+
+                predictions = model.forward(data)
+                loss = loss_function(predictions, data)
+
+                test_loss += loss.item() / len(test_loader)
+                del data
+            test_losses = np.append(test_losses, [[epoch], [test_loss]])
+            writer.add_scalar('Loss/test', test_loss, epoch)
+            save_metrics()
+            torch.save(model.state_dict(), os.path.join(weight_dir, f'cp_{epoch}'))
+save_metrics()
+torch.save(model.state_dict(), os.path.join(weight_dir, 'final'))
